b7167838e2a6af22d605f54e248b10a6bc5d2af1
Abanoub Adel <abanoub.haleem@intigral,net>
2025-01-22 08:39:43 +0200

    add new pipline

--- src/test/java/StepDef/Hooks.java
+++ src/test/java/StepDef/Hooks.java
@@ -3,37 +3,52 @@ package StepDef;
 import io.cucumber.java.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
 
 public class Hooks {
-    public static WebDriver driver;
-    public static Properties prop;
+
+    public static WebDriver driver; // Instance level driver
+    public static Properties prop = new Properties();
     static String configPath = System.getProperty("user.dir") + "\\src\\test\\java\\configuration\\config.properties";
 
-    public Hooks() throws IOException {
-        prop = new Properties();
-        FileInputStream file = new FileInputStream(configPath);
-        prop.load(file);
-    }
-
     @Before
-    public static void startDriver() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver(options);
-        driver.get(prop.getProperty("URL"));
+    public void startDriver() {
+        // Load properties before using them
+        try {
+            FileInputStream file = new FileInputStream(configPath);
+            prop.load(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        WebDriverManager.firefoxdriver().setup();
+        System.out.println("firefoxdriver is set up.");
+        driver = new FirefoxDriver();  // Initialize the WebDriver instance
+        String url = prop.getProperty("URL");
+        System.out.println("url ::" + url);
+        if (url != null && !url.isEmpty()) {
+            driver.get(url);
+        } else {
+            System.out.println("URL is not defined in config.properties");
+        }
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
     }
 
     @After
-    public static void tearDown() {
-        driver.quit();
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    // Getter method for driver if needed in other steps
+    public WebDriver getDriver() {
+        return driver;
     }
 }
