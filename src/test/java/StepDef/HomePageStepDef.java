44d251da951f2601ba8c34d3889b7336bd161d35
Abanoub Adel <abanoub.haleem@intigral,net>
2025-01-22 15:16:36 +0200

    un comment cucumber test

--- src/test/java/StepDef/HomePageStepDef.java
+++ src/test/java/StepDef/HomePageStepDef.java
@@ -1,47 +1,47 @@
-//package StepDef;
-//
-//import io.cucumber.java.en.And;
-//import io.cucumber.java.en.Given;
-//import io.cucumber.java.en.Then;
-//import io.cucumber.java.en.When;
-//import org.openqa.selenium.By;
-//import org.pages.HomePage;
-//import org.testng.Assert;
-//import java.io.IOException;
-//import static StepDef.Hooks.driver;
-//
-//public class HomePageStepDef {
-//
-//    HomePage homePage;
-//    @Given("user in Home Page")
-//    public void userInHomePage() throws IOException {
-//        homePage = new HomePage(driver);
-//    }
-//
-//    @When("user open country list")
-//    public void userOpenCountryList() {
-//        driver.findElement(homePage.getListOfCountryArrow()).click();
-//    }
-//
-//    @And("user select country {string}")
-//    public void userSelectCountry(String nameOfCountry) {
-//        driver.findElement(By.cssSelector(nameOfCountry)).click();
-//    }
-//
-//    @Then("verify price for subscription is {string}")
-//    public void verifyPriceForSubscriptionIs(String priceOFSubscription) {
-//        Assert.assertEquals(
-//                driver.findElement(homePage.getPriceOFSubscription()).getText(), priceOFSubscription);
-//    }
-//
-//    @Then("verify Type for subscription is {string}")
-//    public void verifyTypeForSubscriptionIs(String typeOFSubscription) {
-//        Assert.assertEquals(
-//                driver.findElement(homePage.getSubscribeType()).getText(), typeOFSubscription);
-//    }
-//    @Then("verify currency for subscription is {string}")
-//    public void verifyCurrencyForSubscriptionIs(String currencyOFSubscription) {
-//        Assert.assertTrue(
-//                driver.findElement(homePage.getCurrencyOFSubscription()).getText().contains(currencyOFSubscription));
-//    }
-//}
+package StepDef;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.pages.HomePage;
+import org.testng.Assert;
+import java.io.IOException;
+import static StepDef.Hooks.driver;
+
+public class HomePageStepDef {
+
+    HomePage homePage;
+    @Given("user in Home Page")
+    public void userInHomePage() throws IOException {
+        homePage = new HomePage(driver);
+    }
+
+    @When("user open country list")
+    public void userOpenCountryList() {
+        driver.findElement(homePage.getListOfCountryArrow()).click();
+    }
+
+    @And("user select country {string}")
+    public void userSelectCountry(String nameOfCountry) {
+        driver.findElement(By.cssSelector(nameOfCountry)).click();
+    }
+
+    @Then("verify price for subscription is {string}")
+    public void verifyPriceForSubscriptionIs(String priceOFSubscription) {
+        Assert.assertEquals(
+                driver.findElement(homePage.getPriceOFSubscription()).getText(), priceOFSubscription);
+    }
+
+    @Then("verify Type for subscription is {string}")
+    public void verifyTypeForSubscriptionIs(String typeOFSubscription) {
+        Assert.assertEquals(
+                driver.findElement(homePage.getSubscribeType()).getText(), typeOFSubscription);
+    }
+    @Then("verify currency for subscription is {string}")
+    public void verifyCurrencyForSubscriptionIs(String currencyOFSubscription) {
+        Assert.assertTrue(
+                driver.findElement(homePage.getCurrencyOFSubscription()).getText().contains(currencyOFSubscription));
+    }
+}
--- src/test/java/StepDef/Hooks.java
+++ src/test/java/StepDef/Hooks.java
@@ -1,54 +1,54 @@
-//package StepDef;
-//
-//import io.cucumber.java.*;
-//import io.github.bonigarcia.wdm.WebDriverManager;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.firefox.FirefoxDriver;
-//
-//import java.io.FileInputStream;
-//import java.io.IOException;
-//import java.time.Duration;
-//import java.util.Properties;
-//
-//public class Hooks {
-//
-//    public static WebDriver driver; // Instance level driver
-//    public static Properties prop = new Properties();
-//    static String configPath = System.getProperty("user.dir") + "\\src\\test\\java\\configuration\\config.properties";
-//
-//    @Before
-//    public void startDriver() {
-//        // Load properties before using them
-//        try {
-//            FileInputStream file = new FileInputStream(configPath);
-//            prop.load(file);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        WebDriverManager.firefoxdriver().setup();
-//        System.out.println("firefoxdriver is set up.");
-//        driver = new FirefoxDriver();  // Initialize the WebDriver instance
-//        String url = prop.getProperty("URL");
-//        System.out.println("url ::" + url);
-//        if (url != null && !url.isEmpty()) {
-//            driver.get(url);
-//        } else {
-//            System.out.println("URL is not defined in config.properties");
-//        }
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-//        driver.manage().window().maximize();
-//    }
-//
-//    @After
-//    public void tearDown() {
-//        if (driver != null) {
-//            driver.quit();
-//        }
-//    }
-//
-//    // Getter method for driver if needed in other steps
-//    public WebDriver getDriver() {
-//        return driver;
-//    }
-//}
+package StepDef;
+
+import io.cucumber.java.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+public class Hooks {
+
+    public static WebDriver driver; // Instance level driver
+    public static Properties prop = new Properties();
+    static String configPath = System.getProperty("user.dir") + "\\src\\test\\java\\configuration\\config.properties";
+
+    @Before
+    public void startDriver() {
+        // Load properties before using them
+        try {
+            FileInputStream file = new FileInputStream(configPath);
+            prop.load(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        WebDriverManager.firefoxdriver().setup();
+        System.out.println("firefoxdriver is set up.");
+        driver = new FirefoxDriver();  // Initialize the WebDriver instance
+        String url = prop.getProperty("URL");
+        System.out.println("url ::" + url);
+        if (url != null && !url.isEmpty()) {
+            driver.get(url);
+        } else {
+            System.out.println("URL is not defined in config.properties");
+        }
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    // Getter method for driver if needed in other steps
+    public WebDriver getDriver() {
+        return driver;
+    }
+}
