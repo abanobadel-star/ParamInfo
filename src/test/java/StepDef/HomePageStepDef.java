45d0c187ee54310ffd32cf9208b3fa645b8c6b57
Abanoub Adel <abanoub.haleem@intigral,net>
2025-01-26 12:00:04 +0200

    modify project to enable parallel execution

--- src/test/java/StepDef/HomePageStepDef.java
+++ src/test/java/StepDef/HomePageStepDef.java
@@ -15,41 +15,41 @@ import static StepDef.Hooks.startDriver;
 
 public class HomePageStepDef {
 
-    HomePage homePage;
-    Hooks hooks ;
-    public HomePageStepDef() {
-    }
-
-    @Given("user in Home Page")
-    public void userInHomePage() throws IOException {
-        hooks = new Hooks();
-        homePage = new HomePage(driver);
-    }
-
-    @When("user open country list")
-    public void userOpenCountryList() {
-        driver.findElement(homePage.getListOfCountryArrow()).click();
-    }
-
-    @And("user select country {string}")
-    public void userSelectCountry(String nameOfCountry) {
-        driver.findElement(By.cssSelector(nameOfCountry)).click();
-    }
-
-    @Then("verify price for subscription is {string}")
-    public void verifyPriceForSubscriptionIs(String priceOFSubscription) {
-        Assert.assertEquals(
-            driver.findElement(homePage.getPriceOFSubscription()).getText(), priceOFSubscription);
-    }
-
-    @Then("verify Type for subscription is {string}")
-    public void verifyTypeForSubscriptionIs(String typeOFSubscription) {
-        Assert.assertEquals(
-            driver.findElement(homePage.getSubscribeType()).getText(), typeOFSubscription);
-    }
-    @Then("verify currency for subscription is {string}")
-    public void verifyCurrencyForSubscriptionIs(String currencyOFSubscription) {
-        Assert.assertTrue(
-            driver.findElement(homePage.getCurrencyOFSubscription()).getText().contains(currencyOFSubscription));
-    }
+//    HomePage homePage;
+//    Hooks hooks ;
+//    public HomePageStepDef() {
+//    }
+//
+//    @Given("user in Home Page")
+//    public void userInHomePage() throws IOException {
+//        hooks = new Hooks();
+//        homePage = new HomePage(driver);
+//    }
+//
+//    @When("user open country list")
+//    public void userOpenCountryList() {
+//        driver.findElement(homePage.getListOfCountryArrow()).click();
+//    }
+//
+//    @And("user select country {string}")
+//    public void userSelectCountry(String nameOfCountry) {
+//        driver.findElement(By.cssSelector(nameOfCountry)).click();
+//    }
+//
+//    @Then("verify price for subscription is {string}")
+//    public void verifyPriceForSubscriptionIs(String priceOFSubscription) {
+//        Assert.assertEquals(
+//            driver.findElement(homePage.getPriceOFSubscription()).getText(), priceOFSubscription);
+//    }
+//
+//    @Then("verify Type for subscription is {string}")
+//    public void verifyTypeForSubscriptionIs(String typeOFSubscription) {
+//        Assert.assertEquals(
+//            driver.findElement(homePage.getSubscribeType()).getText(), typeOFSubscription);
+//    }
+//    @Then("verify currency for subscription is {string}")
+//    public void verifyCurrencyForSubscriptionIs(String currencyOFSubscription) {
+//        Assert.assertTrue(
+//            driver.findElement(homePage.getCurrencyOFSubscription()).getText().contains(currencyOFSubscription));
+//    }
 }
